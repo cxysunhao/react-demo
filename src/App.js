4f89803c46f7b3b2acf6ff8ba06ce63eef8c759e
cxysunhao <cxysunhao@163.com>
2021-08-31 09:07:39 +0800

    form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {  HashRouter, Switch, Route } from 'react-router-dom'
+import './App.css'
+import Admin from './pages/admin/index.jsx'
+import Login from './pages/login/index.jsx'
 function App() {
   return (
-    <div>
-      HELLO WORLD
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route path='/admin' component={Admin}/>
+        <Route path='/' component={Login}/>
+      </Switch>
+    </HashRouter>
   );
-}
+} 
 
 export default App;
